c3e5d3da8a09329336c4b656d57e29b0431154ab
Ergis <gisihasani95@gmail.com>
2024-12-23 21:47:36 +0100

    updates

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'npx cap add androidnpx cap add ios',
+  webDir: 'build'
+};
+
+export default config;
