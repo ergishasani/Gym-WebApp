e923cc5599be603d50c8ed4ab77fbad6c0022ca5
Ergis <gisihasani95@gmail.com>
2024-12-27 13:55:04 +0100

    remade

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Update with your backend's base URL
-  timeout: 10000,
-});
-
-export default api;
